7f7e1a8f1ed4784b74e6e87d57a16a797c227906
Marko Mäkelä <marko.makela@mariadb.com>
2021-11-02 08:05:30 +0200

    fixup! 10c3126493d1c4b45953302e44527f17aeca4de9

--- storage/innobase/include/mtr0log.h
+++ storage/innobase/include/mtr0log.h
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 2019, 2020, MariaDB Corporation.
+Copyright (c) 2019, 2021, MariaDB Corporation.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
